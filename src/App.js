338054c614102a5d8ab8828d5f9030a913d9ae84
Ana-Maria Ungureanu <ana-maria.ungureanu@zaplabs.com>
2019-01-10 10:38:01 -0800

    weather app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Card from './Components/card';
+import appId from './appid';
 
 class App extends Component {
+  constructor() {
+    super() 
+    this.state = {
+      weatherData: []
+    }
+  }
+
+  getData(nextFiveDays) {
+    const days = {
+      0: 'Sun',
+      1: 'Mon',
+      2: 'Tue',
+      3: 'Wed',
+      4: 'Thu',
+      5: 'Fri',
+      6: 'Sat'
+    }
+    return nextFiveDays.map((day) => {
+      const dt = new Date(day.dt_txt);
+      const dayOfWeek = dt.getDay();
+      const { main, weather } = day
+      return { day: days[dayOfWeek], main, weather }
+    })
+  }
+
+  async componentDidMount() {
+    try {
+      const weather = await fetch(`http://api.openweathermap.org/data/2.5/forecast?q=Los Angeles&units=imperial&appid=${appId}`)
+      const json = await weather.json()
+      const nextFiveDays = [json.list[0], json.list[8], json.list[16], json.list[24], json.list[32]];
+      const weatherData = this.getData(nextFiveDays)
+      this.setState({ weatherData })
+      console.log(this.state)
+    } catch(err) {
+      throw new Error(err)
+    }
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        {this.state.weatherData.map((data, i)=> {
+          return <Card key={i} day={data.day} high={data.main.temp_max} low={data.main.temp_min} icon={data.weather[0].icon}/>
+        })} 
       </div>
     );
   }
---
+++ src/Components/card.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+
+class Card extends Component {
+  
+  render() {
+    const { day, high, low, icon } = this.props;
+    const iconImg = `http://openweathermap.org/img/w/${icon}.png`;
+      return (
+          <div className="weather-card">
+              <DayName day={day}/>
+              <DayImg icon={iconImg}/>
+              <HighLow high={high} low={low}/>
+          </div>
+      )
+  }
+}
+
+const DayName = ({day}) => (
+  <div className="day">{day}</div>
+)
+
+const DayImg = ({icon}) => (
+  <img className="weather-img" src={icon} alt="weather pic"></img>
+)
+
+const HighLow = ({high, low}) => (
+  <span className="high-low">
+    <span className="high">{Math.round(high)}</span>
+    <span className="low">{Math.round(low)}</span>
+  </span>
+)
+
+export default Card;
