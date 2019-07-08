8e178d6e9f6eb6b77741fe2fadc18a7af5862f12
Ana-Maria Ungureanu <ana-maria.ungureanu@zaplabs.com>
2019-07-08 12:51:31 -0700

    weather app

--- src/Components/Plot/index.js
+++ src/Components/Plot/index.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 // import { Plotly } from 'plotly';
-var Plotly = require('plotly')('aungureanu', 'EqbdMrruEtiblI7ee76m');
+
 
 class Plot extends Component {
 
